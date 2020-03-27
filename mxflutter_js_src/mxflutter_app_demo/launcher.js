e2e636489a3474ed8af358819801d93a940dad31
lucaliu <lucaliu@tencent.com>
2020-03-27 18:58:32 +0800

    删除无用文件

--- mxflutter_js_src/mxflutter_app_demo/launcher.js
+++
@@ -1,9 +0,0 @@
-let rootWidget = require("home_page.js");
-
-function createJSRootWidget(widgetName) {
-    let widget = new rootWidget.JSWidgetHomePage();
-    widget.name = widgetName;
-    return widget;
-}
-
-exports.createJSRootWidget = createJSRootWidget;
