445a8768d91f2b917333f82236d51e069cfb2343
soap <soapreg@qq.com>
2020-03-31 17:48:43 +0800

    -Android支持 -提供基本可用版本 -优化代码结构 -修复若干bug

--- android/app/src/main/java/com/imatrixteam/jsflutter/MXJSEngine.java
+++ android/app/src/main/java/com/imatrixteam/jsflutter/MXJSEngine.java
@@ -10,16 +10,20 @@ import com.eclipsesource.v8.JavaVoidCallback;
 import com.eclipsesource.v8.V8Array;
 import com.eclipsesource.v8.V8Object;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class MXJSEngine {
     static private String TAG = "MXJSEngine";
 
     public MXJSExecutor jsExecutor;
+
+    private MXJSFlutterEngine mMXJSFlutterEngine;
 
     private ArrayList<String> searchDirArray;
 
@@ -27,10 +31,9 @@ public class MXJSEngine {
 
     private Context mContext;
 
-    private Map<String, String[]> dirMap = new HashMap<>();
-
-    public MXJSEngine(Context context) {
+    public MXJSEngine(Context context, MXJSFlutterEngine mxjsFlutterEngine) {
         mContext = context;
+        this.mMXJSFlutterEngine = mxjsFlutterEngine;
         this.searchDirArray = new ArrayList<>();
         this.runnedScriptFile = new HashSet<>();
         setup();
@@ -64,6 +67,54 @@ public class MXJSEngine {
         };
         jsExecutor.registerJavaMethod(dartPrint, "dartPrint");
 
+        JavaVoidCallback setTimeout = new JavaVoidCallback() {
+            @Override
+            public void invoke(V8Object v8Object, V8Array args) {
+                if (args.length() > 1) {
+                    jsExecutor.executeDelay(new MXJSExecutor.MXJsTask() {
+                        @Override
+                        public void excute() {
+                            v8Object.getRuntime().executeScript(args.get(1).toString() + "()");
+                        }
+                    }, Long.parseLong(args.get(1).toString()), TimeUnit.MILLISECONDS);
+                }
+            }
+        };
+        jsExecutor.registerJavaMethod(setTimeout, "setTimeout");
+
+        JavaVoidCallback mx_jsbridge_MethodChannel_invokeMethod = new JavaVoidCallback() {
+            @Override
+            public void invoke(V8Object v8Object, V8Array args) {
+                if (args.length() > 3) {
+                    try {
+                        String channelName = args.get(0).toString();
+                        String methodName = args.get(1).toString();
+                        JSONObject params = new JSONObject(args.get(2).toString());
+                        String function = args.get(3).toString();
+                        mMXJSFlutterEngine.callFlutterMethodChannelInvoke(channelName, methodName, params, new MXJSFlutterEngine.JsMethodChannelCallback() {
+                            @Override
+                            public void callback() {
+                                //todo
+                            }
+                        });
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        jsExecutor.registerJavaMethod(mx_jsbridge_MethodChannel_invokeMethod, "mx_jsbridge_MethodChannel_invokeMethod");
+
+        JavaVoidCallback mx_jsbridge_EventChannel_receiveBroadcastStream_listen = new JavaVoidCallback() {
+            @Override
+            public void invoke(V8Object v8Object, V8Array args) {
+                if (args.length() > 0) {
+                    //todo
+                }
+            }
+        };
+        jsExecutor.registerJavaMethod(mx_jsbridge_EventChannel_receiveBroadcastStream_listen, "mx_jsbridge_EventChannel_receiveBroadcastStream_listen");
+
         JavaCallback require = new JavaCallback() {
             @Override
             public Object invoke(V8Object v8Object, V8Array args) {
@@ -71,9 +122,12 @@ public class MXJSEngine {
                     String filePath = args.get(0).toString();
 
                     String prefix = "./";
-                    if(filePath.startsWith(prefix)){
+                    if (filePath.startsWith(prefix)) {
                         filePath = filePath.substring(prefix.length());
                     }
+
+                    String[] filePathSplitList = filePath.split("/");
+                    int filePathDeep = filePathSplitList.length;
 
                     String absolutePath = "";
 
@@ -81,37 +135,50 @@ public class MXJSEngine {
                     extensions.add(".js");
                     extensions.add(".ddc.js");
 
-                    for (String dir : searchDirArray
-                    ) {
-                        try {
-                            if (!dirMap.containsKey(dir)) {
-                                String[] files = mContext.getAssets().list(dir);
-                                dirMap.put(dir, files);
-                            }
+                    try {
+                        for (int i = 0; i < searchDirArray.size(); i++) {
+                            String curSearchDir = searchDirArray.get(i);
 
-                            for (String fileName : dirMap.get(dir)
-                            ) {
-                                for (String ext : extensions) {
-                                    String absolutePathTemp = dir + "/" + filePath;
-                                    if (!filePath.endsWith(".js")) {
-                                        absolutePathTemp += ext;
-                                        filePath += ext;
+                            for (int j = 0; j < filePathDeep; j++) {
+                                String[] files = mContext.getAssets().list(curSearchDir);
+                                boolean isFind = false;
+                                for (int k = 0; k < files.length; k++) {
+                                    if(j == filePathDeep -1){
+                                        for (String ext : extensions) {
+                                            if (!filePathSplitList[j].endsWith(".js")) {
+                                                filePathSplitList[j] += ext;
+                                                filePath += ext;
+                                            }
+                                            if (filePathSplitList[j].equals(files[k])) {
+                                                curSearchDir += ("/"+filePathSplitList[j]);
+                                                absolutePath = curSearchDir;
+                                                isFind = true;
+                                                break;
+                                            }
+                                        }
+                                    }else if (filePathSplitList[j].equals(files[k])) {
+                                        curSearchDir += ("/"+filePathSplitList[j]);
+                                        isFind = true;
                                     }
-                                    if (fileName.equals(filePath)) {
-                                        absolutePath = absolutePathTemp;
+                                    if(isFind){
                                         break;
                                     }
                                 }
+                                if(!isFind){
+                                    break;
+                                }
                             }
-
-                        } catch (Exception e) {
-                            Log.e(TAG, "", e);
+                            if(!TextUtils.isEmpty(absolutePath)){
+                                break;
+                            }
                         }
+                    } catch (Exception e) {
+                        Log.e(TAG, "", e);
                     }
-                    JSModule module = null;
+                    V8Object exports = null;
                     if (!TextUtils.isEmpty(absolutePath)) {
-                        module = JSModule.require(filePath, absolutePath, jsExecutor.runtime);
-                        if (module == null) {
+                        exports = JSModule.require(filePath, absolutePath, jsExecutor.runtime);
+                        if (exports == null) {
                             jsExecutor.executeScript("throw 'not found'", new MXJSExecutor.ExecuteScriptCallback() {
                                 @Override
                                 public void onComplete(Object value) {
@@ -121,12 +188,20 @@ public class MXJSEngine {
                             return null;
                         }
                     }
-                    return module.mExports;
+                    return exports;
                 }
                 return null;
             }
         };
         jsExecutor.registerJavaMethod(require, "require");
+
+
+        jsExecutor.execute(new MXJSExecutor.MXJsTask() {
+            @Override
+            public void excute() {
+                JSModule.initGlobalModuleCache(jsExecutor.runtime);
+            }
+        });
     }
 
     void addSearchDir(String dir) {
@@ -135,4 +210,5 @@ public class MXJSEngine {
         }
         searchDirArray.add(dir);
     }
+
 }
