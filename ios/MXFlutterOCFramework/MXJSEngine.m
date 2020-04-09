e08be4ab426dcbf4bced2367de8edf644400b772
chaodong <chaodong@tencent.com>
2020-04-09 14:04:16 +0800

    require增加对.lib.js的支持

--- ios/MXFlutterOCFramework/MXJSEngine.m
+++ ios/MXFlutterOCFramework/MXJSEngine.m
@@ -88,7 +88,7 @@
         }
         
         NSString *absolutePath = @"";
-        NSArray *extensions = @[@".js",@".ddc.js"];
+        NSArray *extensions = @[@".js",@".ddc.js",@".lib.js"];
         for(NSString *dir in weakSelf.searchDirArray) {
             for (NSString *extension in extensions) {
                 NSString *absolutePathTemp = [dir stringByAppendingPathComponent:filePath];
