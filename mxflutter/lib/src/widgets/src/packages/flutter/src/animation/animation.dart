6108f9878a1f99a695a62604ffd3ffc8afe736f6
haroldyhli <haroldyhli@tencent.com>
2020-08-12 15:05:04 +0800

    no message

--- mxflutter/lib/src/widgets/src/packages/flutter/src/animation/animation.dart
+++ mxflutter/lib/src/widgets/src/packages/flutter/src/animation/animation.dart
@@ -9,28 +9,29 @@ import 'package:flutter/src/animation/animation.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/src/animation/tween.dart';
 
-
 ///把自己能处理的类注册到分发器中
 Map<String, MXFunctionInvoke> registerAnimationSeries() {
   var m = <String, MXFunctionInvoke>{};
   m[_animationStatus.funName] = _animationStatus;
   return m;
 }
+
 var _animationStatus = MXFunctionInvoke(
-    "AnimationStatus",
-    ({String name, int index}) => MXAnimationStatus.parse(name, index),
-  );
+  "AnimationStatus",
+  ({String name, int index}) => MXAnimationStatus.parse(name, index),
+);
+
 class MXAnimationStatus {
   static AnimationStatus parse(String name, int index) {
-    switch(name) {
-      case 'AnimationStatus.dismissed': 
-       return AnimationStatus.dismissed;
-      case 'AnimationStatus.forward': 
-       return AnimationStatus.forward;
-      case 'AnimationStatus.reverse': 
-       return AnimationStatus.reverse;
-      case 'AnimationStatus.completed': 
-       return AnimationStatus.completed;
+    switch (name) {
+      case 'AnimationStatus.dismissed':
+        return AnimationStatus.dismissed;
+      case 'AnimationStatus.forward':
+        return AnimationStatus.forward;
+      case 'AnimationStatus.reverse':
+        return AnimationStatus.reverse;
+      case 'AnimationStatus.completed':
+        return AnimationStatus.completed;
     }
     return null;
   }
