d1e6bf4e3391010e96b06acdb349b2a8798f0de5
lucaliu <lucaliu@tencent.com>
2020-08-17 15:40:54 +0800

    删除以前dart_sdk.js的桥接类

--- mxflutter/js_lib/framework/dart_js_framework/mx_packages/flutter/src/services/platform_channel.dart.lib.js
+++
@@ -1,9 +0,0 @@
-//  MXFlutterFramework
-//  Copyright 2019 The MXFlutter Authors. All rights reserved.
-//
-//  Use of this source code is governed by a MIT-style license that can be
-//  found in the LICENSE file.
-
-let {platform_channel} = require("message_channel/js_flutter_message_channel.js");
-
-exports.src__services__platform_channel = platform_channel;
