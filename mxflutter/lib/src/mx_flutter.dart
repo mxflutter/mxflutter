d49b91d48a5acddffc1747d79d2d37ee074ce77d
lucaliu <lucaliu@tencent.com>
2020-10-29 19:37:12 +0800

    优化js加载时机

--- mxflutter/lib/src/mx_flutter.dart
+++ mxflutter/lib/src/mx_flutter.dart
@@ -54,11 +54,50 @@ abstract class MXJSFlutter {
   ///@returns void
   ///@throws Error if Path error
   ///
-  runJSApp(
+  static runJSApp(
       {String jsAppPath = "",
       String jsAppAssetsKey = "mxflutter_js_src",
       List<String> jsAppSearchPathList,
-      List<String> jsAppSearchPathWithAssetsKeyList});
+      List<String> jsAppSearchPathWithAssetsKeyList}) {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    // 加载main.js
+    _callNativeRunJSApp(
+        jsAppPath: jsAppPath,
+        jsAppAssetsKey: jsAppAssetsKey,
+        jsAppSearchPathList: jsAppSearchPathList,
+        jsAppSearchPathWithAssetsKeyList: jsAppSearchPathWithAssetsKeyList);
+
+    MXJSFlutter.getInstance();
+  }
+
+  static _callNativeRunJSApp(
+      {String jsAppPath = "",
+      String jsAppAssetsKey = "mxflutter_js_src",
+      List<String> jsAppSearchPathList,
+      List<String> jsAppSearchPathWithAssetsKeyList}) {
+    Map<String, dynamic> args = {
+      "jsAppAssetsKey": jsAppAssetsKey,
+      "jsAppPath": jsAppPath
+    };
+
+    if (jsAppSearchPathList != null) {
+      args["jsAppSearchPathList"] = jsAppSearchPathList;
+    }
+
+    if (jsAppSearchPathWithAssetsKeyList != null) {
+      args["jsAppSearchPathWithAssetsKeyList"] =
+          jsAppSearchPathWithAssetsKeyList;
+    }
+
+    args["flutterAppEnvironmentInfo"] = {
+      "kReleaseMode": kReleaseMode,
+      "kProfileMode": kProfileMode,
+      "kDebugMode": kDebugMode
+    };
+
+    MXPlatformChannel.getInstance().invokeMethod("callNativeRunJSApp", args);
+  }
 
   ///从Flutter Push一个 JS写的页面
   ///@param widgetName: "widgetName",在mxflutter_js_src/main.js  MyApp::createJSWidgetWithName 函数中使用
@@ -75,7 +114,8 @@ abstract class MXJSFlutter {
   ///
   /// '''
   ///
-  dynamic navigatorPushWithName(String widgetName, Key widgetKey, Map flutterPushParams);
+  dynamic navigatorPushWithName(
+      String widgetName, Key widgetKey, Map flutterPushParams);
 
   /// 设置一个处理器，当JS页面加载时，定制Loading widget
   setJSWidgetLoadingHandler(MXWidgetBuildHandler handler);
@@ -94,18 +134,11 @@ class _MXJSFlutter implements MXJSFlutter {
 
   MXJSFlutterApp currentApp;
   bool _isSetup = false;
-  int _mxInitStartTime;
 
   setup() {
     if (_isSetup) {
       return;
     }
-
-    // 记录框架启动开始时间
-    _mxInitStartTime = new DateTime.now().millisecondsSinceEpoch;
-
-    // 需要WidgetsFlutterBinding已调用
-    WidgetsFlutterBinding.ensureInitialized();
 
     // 初始化JS Bridge
     MXJSBridge.getInstance();
@@ -113,50 +146,13 @@ class _MXJSFlutter implements MXJSFlutter {
     // 注册可以被JS调用的方法
     mxRegisterMirrorFunctions();
 
-    _isSetup = true;
-  }
-
-  /// 由Flutter 代码启动JSApp。 可以用在先显示Flutter页面，然后Push路由跳转到JS页面
-  /// 启动JSApp之后，执行JS代码，JS代码可以主动调用Flutter显示自己的页面，也能接受Flutter的指令，显示对应页面
-  ///
-  /// @param jsAppPath jsApp root path ，JS业务代码放置在一个文件夹中，并且有main.js文件。jsAppPath和jsAppAssetsKey根据场景二选一
-  /// @param jsAppAssetsKey 使用pubspec.yaml里的AssetsKey配置来设置jsAppPath，默认为flutter工程下，与lib，ios同级目录的mxflutter_js_src/文件夹下
-  /// @param jsAppSearchPathList js App require 的搜索路径，一般无需设置，默认jsApp root path
-  /// @param jsAppSearchPathWithAssetsKeyList js App require 的搜索路径，使用pubspec.yaml里的AssetsKey配置来设置，一般无需设置，默认jsApp root path
-  /// @returns void
-  /// @throws Error if Path error
-  @override
-  runJSApp(
-      {String jsAppPath = "",
-      String jsAppAssetsKey = "mxflutter_js_src",
-      List<String> jsAppSearchPathList,
-      List<String> jsAppSearchPathWithAssetsKeyList}) {
     // 清理flutter侧的对象映射
     _clearMX();
 
-    Map<String, dynamic> args = {
-      "jsAppAssetsKey": jsAppAssetsKey,
-      "jsAppPath": jsAppPath
-    };
-
-    if (jsAppSearchPathList != null) {
-      args["jsAppSearchPathList"] = jsAppSearchPathList;
-    }
-
-    if (jsAppSearchPathWithAssetsKeyList != null) {
-      args["jsAppSearchPathWithAssetsKeyList"] =
-          jsAppSearchPathWithAssetsKeyList;
-    }
-
-    args["flutterAppEnvironmentInfo"] = _flutterAppEnvironmentInfo();
-
-    MXPlatformChannel.getInstance().invokeMethod("callNativeRunJSApp", args);
-
     // 暂时只支持一个
-    currentApp = MXJSFlutterApp(jsAppAssetsKey);
+    currentApp = MXJSFlutterApp();
 
-    // 记录框架启动耗时
-    args["mxFlutterInitCost"] = new DateTime.now().millisecondsSinceEpoch - _mxInitStartTime;
+    _isSetup = true;
   }
 
   Map _flutterAppEnvironmentInfo() {
@@ -172,8 +168,10 @@ class _MXJSFlutter implements MXJSFlutter {
   /// *重要：此API是从Dart侧打开一个JS页面的入口函数，将创建一个RootWidget，MXFlutter 的RootWidget对外只显示一个
   /// 先创建一个空的MXJSStatefulWidget，调用JS，等待JS层widgetData来刷新页面
   @override
-  dynamic navigatorPushWithName(String widgetName, Key widgetKey, Map flutterPushParams) {
-    dynamic jsWidget = currentApp?.navigatorPushWithName(widgetName, widgetKey, flutterPushParams);
+  dynamic navigatorPushWithName(
+      String widgetName, Key widgetKey, Map flutterPushParams) {
+    dynamic jsWidget = currentApp?.navigatorPushWithName(
+        widgetName, widgetKey, flutterPushParams);
     return jsWidget;
   }
 
