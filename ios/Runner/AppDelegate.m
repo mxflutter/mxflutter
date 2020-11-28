7dec5a28415c5e4e634c00ba6cddcf7938426507
soapyang <soapyang@tencent.com>
2020-11-28 23:07:33 +0800

    完善接入开发指引

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -33,50 +33,35 @@ didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 }
 
 - (void)setupMXFlutterJSPath{
-    
-    ///mxflutter  js代码模拟器热重载支持
-    ///把JS文件重定向到本地地址，简单支持模拟器JS文件热重载，
-    ///仅支持模拟器环境使用
-    ///*发布版本的热更新，也使用同样API设置为你的JS代码下载目录
-    
+
+    /// *mxflutter  js代码模拟器热重载支持*
+    /// 把JS文件重定向到mac电脑本地地址，简单支持模拟器JS文件热重载，
+
 #if TARGET_IPHONE_SIMULATOR
-    
-   //业务JS代码的热重载
+
+    //业务JS代码的热重载
     NSString *jsAppPath = nil;
 
-    ///mxflutter framework的开发者用来加快调试，如不需要修改mxflutter jsframework，无需设置jsFramewrokPath
-    NSString *jsFramewrokPath = nil;
-    
     ///1. 可以直接修改定位到绝对路径,但多人协作开发时，每个人路径不同需要，需要都配置，推荐第二种方法，配置PROJECT_DIR
-    ///jsFramewrokPath = @“/Volumes/Data/Work/mxflutter2/mxflutter/js_lib”;
-    ///jsAppPath =  @"/Volumes/Data/Work/mxflutter2/mxflutter_js_bundle"
+    ///jsAppPath =  @"/Volumes/Data/Work/mxflutter2/mxflutter_js_bundle/"
 
     ///2. 可以配置PROJECT_DIR，把JS代码路径地址重定向到开发机iMac地址，用来支持模拟器热重载，支持多人协同，配置PROJECT_DIR方法如下。
     ///XCode -> Build Settings -> Preprocessor Macros ： Debug下增加 PROJECT_DIR=@\""$PROJECT_DIR\/"\"
-    ///这是github https://github.com/TGIF-iMatrix/mxflutter.git 下的 flutter/example/ios 工程为例子，js_lib的
-    ///物理路径配置如下
+    ///这是github https://github.com/TGIF-iMatrix/mxflutter.git 下的 flutter/example/ios 工程为例子
 
 #ifdef PROJECT_DIR
-    
-    //如果定义了PROJECT_DIR，需要根据自己的工程位置调整相对路径../mxflutter_js_src/
+
+    //可以根据自己的工程部署调整相对路径
     jsAppPath = [PROJECT_DIR stringByAppendingPathComponent:@"../mxflutter_js_bundle/"];
 
-    //如果定义了PROJECT_DIR，需要根据自己的工程位置调整js_lib/的相对路径 ../mxflutter/js_lib/
-    jsFramewrokPath = [PROJECT_DIR stringByAppendingPathComponent:@"../mxflutter/js_lib/"];
-    
 #endif
 
-    //可以调用[MXFlutterPlugin setJSFramewrokPath:jsFramewrokPath]; 设置jsframework到Documents你的下载目录，支持framework的热更新
-    if (jsFramewrokPath.length > 0) {
-         [MXFlutterPlugin setJSFramewrokPath:jsFramewrokPath];
-    }
-    
     if (jsAppPath.length >  0) {
         [MXFlutterPlugin setJSAppPath:jsAppPath jsAppSearchPathList:nil];
     }
-   
+
 #endif
- 
+
 }
 
 //示例代码，非运行mxflutter的必须代码，支持JS的MessageChannel例子
